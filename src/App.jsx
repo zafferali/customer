4ed3ed635f57a47fc624b8e12cfc72fc275eb3a9
zafferali <zafferali@hotmail.com>
2024-08-31 17:45:42 +0530

    app.jsx syntax issue fox

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,118 @@
+// import { useState, useEffect } from 'react';
+// import { StyleSheet, AppState } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+// import { setCustomText } from 'react-native-global-props';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { useSelector, useDispatch } from 'react-redux';
+// import IntroScreen from 'screens/IntroScreen';
+// import { RESULTS } from 'react-native-permissions';
+// import LocationPermissionScreen from 'screens/LocationPermissionScreen';
+// import { initializeCart } from 'redux/slices/initializeCart';
+// import BottomTabNavigator from './navigators/BottomTabNavigator';
+// import { AuthStackNavigator } from './navigators/AuthStackNavigator';
+// import { checkLocationPermission } from './utils/permissions';
+
+// const customTextProps = {
+//   style: {
+//     fontFamily: 'Inter-SemiBold',
+//     fontSize: 28,
+//     color: 'black',
+//     fontWeight: '600',
+//   },
+// };
+
+// setCustomText(customTextProps);
+
+// const App = () => {
+//   const Stack = createStackNavigator();
+//   const { isAuthenticated, isFirstTime, manualLocation } = useSelector(state => state.authentication);
+//   const [locationPermissionChecked, setLocationPermissionChecked] = useState(false);
+//   const [locationPermissionGranted, setLocationPermissionGranted] = useState(false);
+
+//   useEffect(() => {
+//     checkInitialPermission();
+//     const subscription = AppState.addEventListener('change', handleAppStateChange);
+//     return () => {
+//       subscription.remove();
+//     };
+//   }, []);
+
+//   const checkInitialPermission = async () => {
+//     const status = await checkLocationPermission();
+//     setLocationPermissionGranted(status === RESULTS.GRANTED);
+//     setLocationPermissionChecked(true);
+//   };
+
+//   const handleAppStateChange = nextAppState => {
+//     if (nextAppState === 'active') {
+//       checkInitialPermission();
+//     }
+//   };
+
+//   const handlePermissionGranted = () => {
+//     setLocationPermissionGranted(true);
+//   };
+
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(initializeCart());
+//   }, [dispatch]);
+
+//   return (
+//     <SafeAreaView style={styles.fullWidth}>
+//       <NavigationContainer>
+//         {!isAuthenticated ? (
+//           <AuthStackNavigator />
+//         </NavigationContainer>
+//       </SafeAreaView>
+//     );
+//   }
+
+//   if (isFirstTime) {
+//     return (
+//       <SafeAreaView style={styles.fullWidth}>
+//         <NavigationContainer>
+//           <Stack.Navigator
+//             screenOptions={{
+//               gestureEnabled: true,
+//               ...TransitionPresets.SlideFromRightIOS,
+//               headerShown: false,
+//             }}
+//           >
+//             <Stack.Screen name="IntroScreen" component={IntroScreen} />
+//           </Stack.Navigator>
+//         </NavigationContainer>
+//       </SafeAreaView>
+//     );
+//   }
+
+//   if (!locationPermissionChecked) {
+//     // Show a loading screen or splash screen while checking permissions
+//     return null;
+//   }
+
+//   return (
+//     <SafeAreaView style={styles.fullWidth}>
+//       <NavigationContainer>
+//         {!locationPermissionGranted && !manualLocation ? (
+//           <LocationPermissionScreen onPermissionGranted={handlePermissionGranted} />
+//         ) : (
+//           <BottomTabNavigator />
+//         )}
+//       </NavigationContainer>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   fullWidth: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
 import { useState, useEffect } from 'react';
 import { StyleSheet, AppState } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -30,6 +145,8 @@ const App = () => {
   const [locationPermissionChecked, setLocationPermissionChecked] = useState(false);
   const [locationPermissionGranted, setLocationPermissionGranted] = useState(false);
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
     checkInitialPermission();
     const subscription = AppState.addEventListener('change', handleAppStateChange);
@@ -54,21 +171,9 @@ const App = () => {
     setLocationPermissionGranted(true);
   };
 
-  const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(initializeCart());
   }, [dispatch]);
-
-  return (
-    <SafeAreaView style={styles.fullWidth}>
-      <NavigationContainer>
-        {!isAuthenticated ? (
-          <AuthStackNavigator />
-        </NavigationContainer>
-      </SafeAreaView>
-    );
-  }
 
   if (isFirstTime) {
     return (
@@ -96,7 +201,9 @@ const App = () => {
   return (
     <SafeAreaView style={styles.fullWidth}>
       <NavigationContainer>
-        {!locationPermissionGranted && !manualLocation ? (
+        {!isAuthenticated ? (
+          <AuthStackNavigator />
+        ) : !locationPermissionGranted && !manualLocation ? (
           <LocationPermissionScreen onPermissionGranted={handlePermissionGranted} />
         ) : (
           <BottomTabNavigator />
